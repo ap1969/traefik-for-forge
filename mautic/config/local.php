c9263ffaf97a598e3b4f31f754265ab92fc4d618
Andrew Peacock <andrewpeacock1969@gmail.com>
2025-01-03 00:35:07 +0000

    Added embedbuilder websocket server

---
+++ mautic/config/local.php
@@ -0,0 +1,14 @@
+<?php
+$parameters = array(
+	'db_driver' => 'pdo_mysql',
+	'db_host' => 'host.docker.internal',
+	'db_port' => '3306',
+	'db_name' => 'mautic_db',
+	'db_user' => 'scriptuser',
+	'db_password' => 'Scrg394!',
+	'db_table_prefix' => null,
+	'db_backup_tables' => 0,
+	'db_backup_prefix' => 'bak_',
+	'secret_key' => '282f71436953ed45d8fed2576899763ee3dd3f07f0260802ea3f614b1bb8cd1f',
+	'site_url' => 'http://localhost:8001',
+);
